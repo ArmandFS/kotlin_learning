e5539174daafa6d497b7145703ad10365da2857a
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-07 22:09:55 +0700

    Add files via upload

---
+++ MyClass.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinnew
+
+//this function or 'fun' is how to run the kotlin (in main)
+fun main() {
+    print("Hello World! On Kotlin!")
+    print("This is my first few lines on kotlin, and on Android Studio")
+}
