91cf36bda4b17bf967cbcd8341aa158807df7488
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-12 23:05:36 +0700

    Create 1 - Var,Val,Keywords in Kotlin.kt

---
+++ 1 - Var,Val,Keywords in Kotlin.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinnew
+
+//this function or 'fun' is how to run the kotlin (in main)
+fun main() {
+    //create a new variable
+    // var name = "Kotlin // this is mutable and can be changed
+    val myName = "John" //val is immutable
+    print("Hello $myName")
+}
