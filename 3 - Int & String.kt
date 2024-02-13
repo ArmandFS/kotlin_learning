89ef9b13c5f2d7193fa65c9f81c38401c857d4bd
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-13 10:27:34 +0700

    Create 3 - Int & String.kt

---
+++ 3 - Int & String.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinnew
+
+//this function or 'fun' is how to run the kotlin (in main)
+fun main() {
+    /**
+     * var - mutable
+     * val - immutable
+     * Best practice - In essence, use val > var
+     */
+   //initialize variable
+    val name: String
+    val age: Int
+    name = "James"
+    age = 21
+
+    print("Hello my name is $name I'm $age years old.")
+
+}
