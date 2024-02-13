50e95dff4faae263778173f8decf430dc8501045
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-13 11:29:30 +0700

    Create 6 - Basic Operators.kt

---
+++ 6 - Basic Operators.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinnew
+
+//this function or 'fun' is how to run the kotlin (in main)
+fun main() {
+    //operators
+    //making it double so that it can be divided
+    val a = 23.0
+    val b = 12.0
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    //modulo operator for remainder
+    print(a%b)
+
+}
