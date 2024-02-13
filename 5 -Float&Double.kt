37c6fa2e1ddec36331205f096b0eb3cb4b569063
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-13 11:17:59 +0700

    Create 5 -Float&Double.kt

---
+++ 5 -Float&Double.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinnew
+
+//this function or 'fun' is how to run the kotlin (in main)
+fun main() {
+    /**double and float */
+    val pi = 3.14334
+    val e = 2.7188122
+    //float signified by adding f
+    val eFloat = 2.6123123f
+    print(pi)
+    print(eFloat)
+    
+}
+
