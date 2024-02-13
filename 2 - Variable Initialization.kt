63c51cef9124a3c8c5ffb56212253a1235d12ffd
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-13 10:11:31 +0700

    Create 2 - Variable Initialization.kt

---
+++ 2 - Variable Initialization.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinnew
+
+//this function or 'fun' is how to run the kotlin (in main)
+fun main() {
+    /**
+     * var - mutable
+     * val - immutable
+     * Best practice - In essence, use val > var
+     */
+   //initialize variable
+    val name: String = "James"
+    print("Hello $name")
+
+}
