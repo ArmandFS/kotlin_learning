c6290c024d26f52c2658e202045cb362a291a4cf
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-14 11:21:46 +0700

    Create 9 - basic functions.kt

---
+++ 9 - basic functions.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinnew
+
+//loops and branching
+fun main() {
+    //creating a simple kotlin function
+    iIsmultiple()
+}
+
+fun iIsmultiple(){
+    for (i in 1..100){
+        if(i%2 ==0){
+            println("$i is multiple of 2!")
+        }
+    }
+
+}
