bb724c3427868a89231679d8288d79f3dce6be62
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-15 12:32:19 +0700

    Create 18 - Empty Collections.kt

---
+++ 18 - Empty Collections.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinnew
+
+//collections in kotlin
+fun main() {
+    //empty collections in kotlin
+    val empty = emptyList<String>()
+    val emptySet = emptySet<Int>()
+    val emptyMap = emptyMap<String, Int>()
+}
